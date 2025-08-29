f95dcf298b3487ead4f4169bf6417eedd53efc7b
Badal9924 <badalkr0521bth@gmail.com>
2025-08-29 17:09:50 +0530

    adding fetchexam api

--- src/routes/exam.route.js
+++ src/routes/exam.route.js
@@ -3,6 +3,7 @@ import { verifyJWT } from "../middleware/auth.middleware.js";
 import {
   createExam,
   createQuestion,
+  fetchExams,
   getQuestions,
 } from "../controllers/exam.controller.js";
 
@@ -14,5 +15,6 @@ router.route("/").post(verifyJWT, createExam);
 router.route("/invite").post(verifyJWT, inviteStudent);
 router.route("/question").post(verifyJWT, createQuestion);
 router.route("/question").get(verifyJWT, getQuestions);
-
+router.route("/").get(verifyJWT, fetchExams);
+// 
 export default router;
