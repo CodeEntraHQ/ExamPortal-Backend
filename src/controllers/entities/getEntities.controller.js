a3c7d0f028cdee9482edc2d7cad13a5f8ccd5c02
Rishabh Gautam <44278020+Rishabh7896@users.noreply.github.com>
2025-10-19 00:20:46 +0530

    entity management (#39)

--- src/controllers/entities/getEntities.controller.js
+++ src/controllers/entities/getEntities.controller.js
@@ -1,6 +1,10 @@
 import Entity from "#models/entity.model.js";
+import Exam from "#models/exam.model.js";
+import User from "#models/user.model.js";
 import { ApiHandler } from "#utils/api-handler/handler.js";
 import { ApiResponse } from "#utils/api-handler/response.js";
+import { USER_ROLES } from "#utils/constants/model.constant.js";
+import { constructMediaLink } from "#utils/utils.js";
 
 export const getEntities = ApiHandler(async (req, res) => {
   // Parsing request
@@ -9,12 +13,30 @@ export const getEntities = ApiHandler(async (req, res) => {
   const offset = (page - 1) * limit;
 
   // Fetch entity
-  const { rows: entities, count: total } = await Entity.findAndCountAll({
+  const { rows, count: total } = await Entity.findAndCountAll({
     offset,
     limit,
-    attributes: ["id", "name", "address", "type"],
     order: [["created_at", "ASC"]],
   });
+
+  const updatedRows = await Promise.all(
+    rows.map(async (entity) => ({
+      id: entity.id,
+      address: entity.address,
+      created_at: entity.created_at,
+      description: entity.description,
+      email: entity.email,
+      logo_link: constructMediaLink(entity.logo_id),
+      name: entity.name,
+      phone_number: entity.phone_number,
+      status: "ACTIVE",
+      total_exams: await Exam.count({ where: { entity_id: entity.id } }),
+      total_students: await User.count({
+        where: { entity_id: entity.id, role: USER_ROLES.STUDENT },
+      }),
+      type: entity.type,
+    }))
+  );
 
   // Send response
   return res.status(200).json(
@@ -23,7 +45,7 @@ export const getEntities = ApiHandler(async (req, res) => {
       page,
       limit,
       totalPages: Math.ceil(total / limit),
-      entities,
+      entities: updatedRows,
     })
   );
 });
